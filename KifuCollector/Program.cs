ea4a86226d889a003d4ba5810e561f951fcc89fc
unknown <>
2017-10-27 12:04:32 +0800

    添加项目文件。

---
+++ KifuCollector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+
+
+namespace KifuCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+        }
+    }
+}
