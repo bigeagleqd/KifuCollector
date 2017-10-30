bc396a15d1f59448f52a8f0e0b83c0ad86bf60f9
刘云鹏 <liuyunpeng@liuyunpengdeMacBook-Air.local>
2017-10-30 23:15:06 +0800

    mac

--- KifuCollector/Program.cs
+++ KifuCollector/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/************************************************
+*
+* File: Program.cs
+*
+* Author: bigeagleqd@hotmail.com
+*
+*********************************************/
+
+using System;
 using System.IO;
 using System.Net;
 using System.Linq;
@@ -6,13 +14,62 @@ using System.Linq;
 
 namespace KifuCollector
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            
+            GuessNumber();         
         }
-    }
-}
+        private static void DoSometing()
+        {
+            Console.Write("请输入你的名字：");
+            string s = Console.ReadLine();
+            Console.WriteLine("你的名字是：{0}。", s);
+            Console.ReadKey();
+        
+
+    
+        }
+
+        private static void GuessNumber()
+        {
+            int maxNumber =10000;
+            int minNumber = 0;
+            
+            Console.WriteLine("请在心里想一个不大于{0}的数字。", maxNumber);
+            while(true)
+            {
+                int tempNumber = (maxNumber - minNumber) / 2;
+                Console.Write("这个数大于{0}吗(y/n)？", tempNumber);
+                string s = Console.ReadLine();
+                if(s == "y")
+                {
+                    //maxNumber = tempNumber;
+                    minNumber = tempNumber; 
+
+
+                }
+                else if(s == "n")
+                {
+                    maxNumber = tempNumber;
+                }
+                else
+                {
+                    continue;
+
+                }
+                if(minNumber == maxNumber)
+                {
+                    Console.WriteLine("你想的数是{0}", maxNumber);
+                    break;
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }//end class
+    
+}//end namespace
