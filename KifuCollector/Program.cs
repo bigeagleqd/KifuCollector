5ecfb434b0adf0a46f0d9d4b3e3c9ea8453901fb
刘云鹏 <liuyunpeng@liuyunpengdeMacBook-Air.local>
2017-10-31 21:40:14 +0800

    读棋谱

--- KifuCollector/Program.cs
+++ KifuCollector/Program.cs
@@ -13,6 +13,8 @@ using System.Linq;
 using Bigeagle.Portable.BoardGames;
 using Bigeagle.Portable.BoardGames.Go;
 using System.Text;
+using System.Diagnostics;
+
 namespace KifuCollector
 {
 
@@ -29,11 +31,25 @@ namespace KifuCollector
                 serializer.KiFUEncoding = Encoding.UTF8;
                 using (Stream sr = File.OpenRead(sgfFile))
                 {
-                    GoGameInfo gameInfo = serializer.ReadGameInfo(sr);
-                    Console.WriteLine("name:{0}", gameInfo.Name);
-                    Console.WriteLine("black:{0}", gameInfo.BlackPlayer.Name);
-                    Console.WriteLine("white:{0}", gameInfo.WhitePlayer.Name);
-                    Console.WriteLine("result :{0}", gameInfo.Result.ChineseString);
+                    // GoGameInfo gameInfo = serializer.ReadGameInfo(sr);
+                    // Console.WriteLine("name:{0}", gameInfo.Name);
+                    // Console.WriteLine("black:{0}", gameInfo.BlackPlayer.Name);
+                    // Console.WriteLine("white:{0}", gameInfo.WhitePlayer.Name);
+                    // Console.WriteLine("result :{0}", gameInfo.Result.ChineseString);
+                    byte[] buffer = new byte[sr.Length];
+                    sr.Read(buffer, 0 , buffer.Length);
+                    string s = serializer.KiFUEncoding.GetString(buffer);
+                    #if DEBUG
+                    Console.WriteLine(s);
+                    #endif
+
+
+                    KiFUGame game = serializer.DeSerialize(s);
+                    Console.WriteLine("对局 : {0}", game.GameInfo.Name);
+                    Console.WriteLine("赛事：{0}", game.GameInfo.Event);
+                    Console.WriteLine("black:{0}", game.GameInfo.BlackPlayer.Name);
+                    Console.WriteLine("white:{0}", game.GameInfo.WhitePlayer.Name);
+                    Console.WriteLine("result :{0}", game.GameInfo.Result.ChineseString);
                     sr.Close();
                 }
             }
